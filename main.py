0f5cc1634e62f00ef1b12e1bbc01ae6c46902b3e
jaykybert <kybertjay@gmail.com>
2021-11-23 14:49:08 +0000

    feat: project structure definition, google cloud platform files

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return 'Home'
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
+
