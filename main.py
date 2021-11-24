05d6cbf13b15e10677c47d61e75818159ea7e7c5
jaykybert <51761831+jaykybert@users.noreply.github.com>
2021-11-24 16:36:25 +0000

    feat: home template, app routing, error handling

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/home')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/submitted', methods=['POST'])
+def register_complete():
+    name = request.form['name']
+    email = request.form['email']
+
+    return render_template('register_complete.html',
+                           name=name, email=email)
+
+
+# Page Not Found
+@app.errorhandler(404)
+def error_404(e):
+    return render_template('error.html', error=e)
+
+
+# Internal Server Error
+@app.errorhandler(500)
+def error_500(e):
+    return render_template('error.html', error=e)
 
 
 if __name__ == '__main__':
