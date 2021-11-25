5b5550a6eee19d7de4fc6834bc46d46a017b6cb9
jaykybert <51761831+jaykybert@users.noreply.github.com>
2021-11-25 17:29:54 +0000

    unstable: cloud sql deployment - local not working

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
+# Standard
+import os
+import sys
+# Third-Party
 from flask import Flask, render_template, request
+import pymysql
+# Application-Specific
+if __name__ == '__main__':
+    sys.path.append('local/')
+    import local_credentials
+
 
 app = Flask(__name__)
 
 
+# https://google.github.io/styleguide/pyguide.html
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    # Cloud Deployment
+    if os.environ.get('GAE_ENV') == 'standard':
+        unix_socket = '/cloudsql/{}'.format(db_connection)
+        conn = pymysql.connect(user=db_user, password=db_password,
+                               unix_socket=unix_socket, db=db_database)
+    # Local Deployment - Use TCP connections.
+    else:
+        conn = pymysql.connect(user=db_user, password=db_password,
+                               host='127.0.0.1', db=db_database)
+
+    with conn.cursor() as cur:
+        cur.execute('SELECT * FROM PRODUCT')
+        results = cur.fetchall()
+    conn.close()
+
+    return render_template('home.html', results=results)
 
 
 @app.route('/about')
@@ -39,7 +66,25 @@ def error_500(e):
     return render_template('error.html', error=e)
 
 
+# Local Deployment
 if __name__ == '__main__':
+    print('--------------------- Local Deployment')
+    db = local_credentials.sql_variables()
+    db_user = db['SQL_USERNAME']
+    db_password = db['SQL_PASSWORD']
+    db_database = db['SQL_DATABASE']
+    db_connection = db['SQL_CONNECTION']
+
     app.run(host='127.0.0.1', port=8080, debug=True)
+
+# Cloud Deployment
+else:
+    print('--------------------- Cloud Deployment')
+    db_user = os.environ.get('SQL_USERNAME')
+    db_password = os.environ.get('SQL_PASSWORD')
+    db_database = os.environ.get('SQL_DATABASE')
+    db_connection = os.environ.get('SQL_CONNECTION')
+
+
 
 
