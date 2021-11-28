6e611b58420920ef4d520dbc7dc7677a768e2b06
jaykybert <51761831+jaykybert@users.noreply.github.com>
2021-11-28 20:14:42 +0000

    feat: firebase authentication, python decouple

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
 # Standard
 import os
-import sys
 # Third-Party
 from flask import Flask, render_template, request
 import pymysql
-# Application-Specific
-if __name__ == '__main__':
-    sys.path.append('local/')
-    import local_credentials
-
+# Application
+from decouple import config
 
 app = Flask(__name__)
 
-
 # https://google.github.io/styleguide/pyguide.html
+
 
 @app.route('/')
 def home():
+    results = None
     # Cloud Deployment
     if os.environ.get('GAE_ENV') == 'standard':
         unix_socket = '/cloudsql/{}'.format(db_connection)
         conn = pymysql.connect(user=db_user, password=db_password,
                                unix_socket=unix_socket, db=db_database)
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM PRODUCT')
+            results = cur.fetchall()
+        conn.close()
+
     # Local Deployment - Use TCP connections.
     else:
-        conn = pymysql.connect(user=db_user, password=db_password,
-                               host='127.0.0.1', db=db_database)
-
-    with conn.cursor() as cur:
-        cur.execute('SELECT * FROM PRODUCT')
-        results = cur.fetchall()
-    conn.close()
+        print('No database connection')
 
     return render_template('home.html', results=results)
 
@@ -66,25 +62,20 @@ def error_500(e):
     return render_template('error.html', error=e)
 
 
-# Local Deployment
+# Local Deployment - SQL variables saved in .env.
 if __name__ == '__main__':
     print('--------------------- Local Deployment')
-    db = local_credentials.sql_variables()
-    db_user = db['SQL_USERNAME']
-    db_password = db['SQL_PASSWORD']
-    db_database = db['SQL_DATABASE']
-    db_connection = db['SQL_CONNECTION']
+    db_user = config('SQL_USERNAME')
+    db_password = config('SQL_PASSWORD')
+    db_database = config('SQL_DATABASE')
+    db_connection = config('SQL_CONNECTION')
 
     app.run(host='127.0.0.1', port=8080, debug=True)
 
-# Cloud Deployment
+# Cloud Deployment - SQL variables saved in app.yaml.
 else:
-    print('--------------------- Cloud Deployment')
     db_user = os.environ.get('SQL_USERNAME')
     db_password = os.environ.get('SQL_PASSWORD')
     db_database = os.environ.get('SQL_DATABASE')
     db_connection = os.environ.get('SQL_CONNECTION')
-
-
-
 
---
+++ static/scripts/firebaseConfig.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBjj680whocrHSvpfwrUN7uX8Qz00KVpiI",
+    authDomain: "ad-assignment-21.firebaseapp.com",
+    projectId: "ad-assignment-21",
+    storageBucket: "ad-assignment-21.appspot.com",
+    messagingSenderId: "575699106331",
+    appId: "1:575699106331:web:6a80779f1c8a92c97216be",
+    measurementId: "G-2SJSTKWGBY"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
